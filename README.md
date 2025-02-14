2d12c00b4004eb1d895fbd861a1d57cfa8900a55
kelvinjobbi <kelvin@jobbi.me>
2025-02-13 19:37:03 -0600

    mi portafoli

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Full Stack Developer con React y PHP
+
+🚀 Seminario de Especialización en Ciencias de la Computación ciclo 01-2025
+
+## Proyecto: Crear un Portafolio Personal Responsivo
+
+### ✨ Objetivo del Proyecto:
+El objetivo es que los estudiantes diseñen y desarrollen un sitio web personal donde puedan mostrar sus habilidades, proyectos anteriores, experiencia laboral, y cualquier otro aspecto que les gustaría resaltar para un posible empleador o cliente. El sitio debe ser responsivo, lo que significa que debe verse y funcionar correctamente en cualquier dispositivo (desde un móvil hasta una pantalla grande).
+### ✨ Tecnologías Requeridas:
+- **HTML:** Para estructurar la página (header, footer, secciones, etc.).
+- **CSS:**  Para estilizar la página, aplicando un diseño limpio y profesional.
+- **JavaScript:**  Para interactividad (por ejemplo, un formulario de contacto, animaciones, o una galería de imágenes).
+- **Framework Bootstrap:**  Para mejorar la estructura y diseño de forma rápida, o CSS puro si desean aprender a diseñar desde cero.
+
+## 📚  Recursos utilizados
+
+ - [Bootstrap 5.3](https://getbootstrap.com/)
+ - [Illustraciones SVG](https://undraw.co/illustrations)
+ - [Icons8.com](https://icons8.com/)
+ - [Bootstrap 5.3 Footer](https://getbootstrap.com/docs/5.3/examples/footers/)
+
+## 📂 Estructura del proyecto
+
+La organización de archivos en este proyecto es la siguiente:
+
+- **`index.html`** → Archivo principal del sitio web.  
+- **`/assets/images`** → Contiene todas las imágenes utilizadas.  
+- **`/assets/css`** → Archivos CSS para el diseño y estilos del proyecto.  
+- **`/assets/js`** → Contiene scripts de JavaScript para la funcionalidad.  
+- **`README.md`** → Documentación del proyecto.  
---
+++ assets/js/app.js
@@ -0,0 +1,65 @@
+var habilidades = [
+    { name: "Flutter/Dart", value: 95 },
+    { name: "ASP.NET, .Net Core, C#, Basic.", value: 90 },
+    { name: "FastApi/Python", value: 85 },
+    { name: "React Native/JS", value: 95 },
+    { name: "Laravel", value: 99 },
+    { name: "NextJs", value: 55 },
+    { name: "NestJs", value: 75 },
+    { name: "NodeJS", value: 85 },
+    { name: "Android Java/Kotlin", value: 89 },
+    { name: "PHP", value: 99 },
+    { name: "Docker", value: 69 },
+    { name: "Git/Github/Gitlab", value: 79 },
+    { name: "Mysql, PostgreSQL, MSSQL Server, Firebase, MongoDB", value: 89 },
+    { name: "Vercel", value: 90 },
+    { name: "Supabase", value: 90 },
+    { name: "Figma, Zeplin, Adobe XD", value: 85 },
+];
+
+var contenedor = document.getElementById('load_habilidades');
+const toastLiveExample = document.getElementById('liveToast')
+
+if (habilidades.length > 0) {
+    habilidades.forEach(e => {
+        console.log(e);
+        contenedor.innerHTML += `
+        <div class="col-lg-12">
+        <label for="label" class="fw-bold mt-2">${e.name}</label>
+        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="${e.value}"
+        aria-valuemin="0" aria-valuemax="100">
+        <div class="progress-bar bg-primary" style="width: ${e.value}%">${e.value}%</div>
+        </div>
+        </div>`;
+    });
+}
+
+// validation form
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            event.preventDefault()
+            if (!form.checkValidity()) {
+                event.stopPropagation()
+            } else {
+                try {
+                    const formData = new FormData(form);
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    toastBootstrap.show()
+                    form.reset();
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+
+})()
